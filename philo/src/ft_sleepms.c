96ce6ea9db4693b3673ffcba77a22da447ee6a8b
Martin Cutura <mcutura@student.42berlin.de>
2023-07-10 08:21:26 +0200

    optimize sleep performance

--- philo/src/ft_sleepms.c
+++ philo/src/ft_sleepms.c
@@ -6,7 +6,7 @@
 /*   By: mcutura <mcutura@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 02:06:41 by mcutura           #+#    #+#             */
-/*   Updated: 2023/07/10 01:27:56 by mcutura          ###   ########.fr       */
+/*   Updated: 2023/07/10 08:18:56 by mcutura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_sleepms(int ms)
 	gettimeofday(&start, NULL);
 	while (1)
 	{
-		usleep(50);
+		usleep(500);
 		if (get_elapsed_time(start) >= ms)
 			return ;
 	}
